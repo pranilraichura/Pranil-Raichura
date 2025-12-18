645d96272468690a30d6705a54120c5e3d86b8be
Pranil Raichura <pranil.raichura@gmail.com>
2025-12-17 20:43:18 -0800

    Update landing page text

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function ComingSoon() {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8, delay: 0.2 }}
                 >
-                    coming soon
+                    hidden to the public for now
                 </motion.h1>
 
                 {/* Animated loading dots */}
